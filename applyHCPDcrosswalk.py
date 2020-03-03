48c19785213e899186135f30bca8429aaffa4fe9
Moises Baltazar <me@moisesb.com>
2020-03-03 13:44:46 -0600

    Break down function

--- applyHCPDcrosswalk.py
+++ applyHCPDcrosswalk.py
@@ -12,62 +12,6 @@ from libs.config import LoadSettings
 from libs.redcap import RedcapTable, get_behavioral_ids, get_behavioral
 
 
-pathout = './pathout/'
-def redcap2structure(variables, crosswalk, pathstructuresout=pathout, studystr='hcpa', dframe=None):
-    """
-    Takes list of vars from the crosswalk, gets the data from Redcap, and puts into structure format after
-    merging with NDAR requiredvars.  Outputs a csv structure in NDA format to pathstructureout location
-    """
-    if dframe is not None:
-        studydata = dframe
-    else:
-        studydata = get_behavioral(studystr, variables)
-    # get the relevant rows of the crosswalk
-    crosswalk_subset = crosswalk[crosswalk.hcp_variable.isin(variables)]\
-        [['nda_structure', 'nda_element', 'hcp_variable', 'source',
-         'action_request', 'hcp_variable_upload', 'requested_python']]
-
-    # execute transformation codes stored in the crosswalk
-    # TODO: sanitize input to prevent malicious code injection
-    for livewire in crosswalk_subset.requested_python[crosswalk_subset.requested_python.notna()]:
-        exec(livewire)
-
-    # remove fields with empty values hcp_variable name in uploaded file -- these are empty because NDA doesnt want them
-    crosswalk_subset = crosswalk_subset[crosswalk_subset['hcp_variable_upload'].notna()]
-    listout = ['subject', 'flagged', 'interview_date', 'interview_age', 'gender'] +\
-                crosswalk_subset['hcp_variable_upload'].tolist()
-    # output new variables and subset to those not flagged for withdrawal.
-    transformed = studydata[listout][studydata.flagged.isnull()].drop(
-        columns={'flagged', 'interview_date', 'gender', 'interview_age'})
-    # merge with required fields from vars in intradb staging (guid, etc)
-    # not sure whether it makes sense to pull these in here or recalculate on fly from redcap.
-    # future issues:  compare this approach (e.g. pull from the file above named 'ndar') vs. what happens in the applycrosswalk.py
-    # program for HCD, which regenerates on fly...will require some recodeing below to pull from redcap...
-    # might just be easier to pull once...but how will this affect visit numbers?
-    ndarsub = ndar[['nda_guid', 'subjectped', 'nda_gender', 'nda_interview_age', 'nda_interview_date']].rename(
-        columns={'nda_guid': 'subjectkey', 'subjectped': 'src_subject_id', 'nda_gender': 'gender',
-                 'nda_interview_date': 'interview_date', 'nda_interview_age': 'interview_age'}).copy()
-    dout = pd.merge(ndarsub, transformed, how='left', left_on='src_subject_id', right_on='subject').drop(
-        columns='subject')
-    dout['interview_date'] = pd.to_datetime(dout.interview_date).dt.strftime('%m/%d/%Y')
-    # now export
-    crosswalk_subset.reset_index(inplace=True)
-    strucroot = crosswalk_subset.nda_structure.str.strip().str[:-2][0]
-    strucnum = crosswalk_subset.nda_structure.str.strip().str[-2:][0]
-    # finalsubset - i.e. no withdraws
-    # subjectkey	src_subject_id	interview_age	interview_date	gender
-    filePath = os.path.join(pathstructuresout, 'HCPD_' + strucroot + strucnum + '_' + snapshotdate + '.csv')
-    if os.path.exists(filePath):
-        os.remove(filePath)
-    else:
-        pass
-        # print("Can not delete the file as it doesn't exists")
-
-    with open(filePath, 'a') as f:
-        f.write(strucroot + "," + str(int(strucnum)) + "\n")
-        dout.to_csv(f, index=False)
-
-
 
 # +
 box = LifespanBox()
@@ -90,7 +34,6 @@ pathout = "./prepped_structures"
 structures = crosswalk.drop_duplicates('nda_structure')
 # normal redcap structures
 normals = structures[structures.source.str.contains('REDCap') & structures.specialty_code.isnull()]
-normals
 
 # +
 crosswalk['dbasestring'] = crosswalk.dbase.astype(str)
@@ -100,10 +43,9 @@ crosswalk.loc[crosswalk.dbasestring.str.contains('hcp'), 'countdb'] = crosswalk.
 # parent about child  -- no parent apbout parent.
 # parent about parent data to be dealt with later as extension of this release..i.e. with fu data, v2, and v3
 # -
+normals.nda_structure
 
-
-
-for structure in normals.nda_structure:
+for structure in normals.nda_structure.tolist()[:]:
     current_structure = crosswalk[crosswalk.nda_structure == structure]
     variables = current_structure.hcp_variable.tolist()
     numstudies = current_structure.countdb.values[0]
@@ -134,6 +76,92 @@ for structure in normals.nda_structure:
         studydata = studydata_v2.copy()
 
     redcap2structure(variables, crosswalk, pathstructuresout=pathout, studystr='hcpd', dframe=studydata)
+
+pathstructuresout = pathout
+studystr = 'hcpd'
+dframe = studydata
+
+# 1. Takes list of vars from the crosswalk
+# 2. gets the data from Redcap
+# 3. puts into structure format after merging with NDAR requiredvars
+# 4. Outputs a csv structure in NDA format
+
+crosswalk_subset = crosswalk[crosswalk.hcp_variable.isin(variables)]\
+        [['nda_structure', 'nda_element', 'hcp_variable', 'source',
+         'action_request', 'hcp_variable_upload', 'requested_python']]
+
+for livewire in crosswalk_subset.requested_python[crosswalk_subset.requested_python.notna()]:
+    exec(livewire)
+
+# remove fields with empty values hcp_variable name in uploaded file -- these are empty because NDA doesnt want them
+crosswalk_subset = crosswalk_subset[crosswalk_subset['hcp_variable_upload'].notna()]
+listout = ['subject', 'flagged', 'interview_date', 'interview_age', 'gender'] + crosswalk_subset['hcp_variable_upload'].tolist()
+
+# output new variables and subset to those not flagged for withdrawal.
+transformed = studydata[listout][studydata.flagged.isnull()].drop(
+    columns={'flagged', 'interview_date', 'gender', 'interview_age'})
+
+# merge with required fields from vars in intradb staging (guid, etc)
+# not sure whether it makes sense to pull these in here or recalculate on fly from redcap.
+# future issues:  compare this approach (e.g. pull from the file above named 'ndar') vs. what happens in the applycrosswalk.py
+# program for HCD, which regenerates on fly...will require some recodeing below to pull from redcap...
+# might just be easier to pull once...but how will this affect visit numbers?
+ndarsub = ndar[['nda_guid', 'subjectped', 'nda_gender', 'nda_interview_age', 'nda_interview_date']]\
+    .rename(columns={'nda_guid': 'subjectkey', 'subjectped': 'src_subject_id', 'nda_gender': 'gender',
+             'nda_interview_date': 'interview_date', 'nda_interview_age': 'interview_age'}).copy()
+dout = pd.merge(ndarsub, transformed, how='left', left_on='src_subject_id', right_on='subject')\
+        .drop(columns='subject')
+dout['interview_date'] = pd.to_datetime(dout.interview_date).dt.strftime('%m/%d/%Y')
+
+# +
+# now export
+crosswalk_subset.reset_index(inplace=True)
+strucroot = crosswalk_subset.nda_structure.str.strip().str[:-2][0]
+strucnum = crosswalk_subset.nda_structure.str.strip().str[-2:][0]
+# finalsubset - i.e. no withdraws
+# subjectkey	src_subject_id	interview_age	interview_date	gender
+filePath = os.path.join(pathstructuresout, 'HCPD_' + strucroot + strucnum + '_' + snapshotdate + '.csv')
+if os.path.exists(filePath):
+    os.remove(filePath)
+else:
+    pass
+    # print("Can not delete the file as it doesn't exists")
+
+with open(filePath, 'a') as f:
+    f.write(strucroot + "," + str(int(strucnum)) + "\n")
+    dout.to_csv(f, index=False)
+# -
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+studydata.columns[studydata.columns.str.contains('___')]
+
+studydata_v2.columns
+
+
+
+
 
 
 
--- crosswalkHelper.py
+++ crosswalkHelper.py
@@ -21,3 +21,58 @@ def extraheightcleanvar(dfnewchildold):
                             'height']].copy()
     return cleandata
 
+pathout = './pathout/'
+def redcap2structure(variables, crosswalk, pathstructuresout=pathout, studystr='hcpa', dframe=None):
+    """
+    Takes list of vars from the crosswalk, gets the data from Redcap, and puts into structure format after
+    merging with NDAR requiredvars.  Outputs a csv structure in NDA format to pathstructureout location
+    """
+    if dframe is not None:
+        studydata = dframe
+    else:
+        studydata = get_behavioral(studystr, variables)
+    # get the relevant rows of the crosswalk
+    crosswalk_subset = crosswalk[crosswalk.hcp_variable.isin(variables)]\
+        [['nda_structure', 'nda_element', 'hcp_variable', 'source',
+         'action_request', 'hcp_variable_upload', 'requested_python']]
+
+    # execute transformation codes stored in the crosswalk
+    # TODO: sanitize input to prevent malicious code injection
+    for livewire in crosswalk_subset.requested_python[crosswalk_subset.requested_python.notna()]:
+        print(livewire)
+        exec(livewire)
+
+    # remove fields with empty values hcp_variable name in uploaded file -- these are empty because NDA doesnt want them
+    crosswalk_subset = crosswalk_subset[crosswalk_subset['hcp_variable_upload'].notna()]
+    listout = ['subject', 'flagged', 'interview_date', 'interview_age', 'gender'] +\
+                crosswalk_subset['hcp_variable_upload'].tolist()
+    # output new variables and subset to those not flagged for withdrawal.
+    transformed = studydata[listout][studydata.flagged.isnull()].drop(
+        columns={'flagged', 'interview_date', 'gender', 'interview_age'})
+    # merge with required fields from vars in intradb staging (guid, etc)
+    # not sure whether it makes sense to pull these in here or recalculate on fly from redcap.
+    # future issues:  compare this approach (e.g. pull from the file above named 'ndar') vs. what happens in the applycrosswalk.py
+    # program for HCD, which regenerates on fly...will require some recodeing below to pull from redcap...
+    # might just be easier to pull once...but how will this affect visit numbers?
+    ndarsub = ndar[['nda_guid', 'subjectped', 'nda_gender', 'nda_interview_age', 'nda_interview_date']].rename(
+        columns={'nda_guid': 'subjectkey', 'subjectped': 'src_subject_id', 'nda_gender': 'gender',
+                 'nda_interview_date': 'interview_date', 'nda_interview_age': 'interview_age'}).copy()
+    dout = pd.merge(ndarsub, transformed, how='left', left_on='src_subject_id', right_on='subject').drop(
+        columns='subject')
+    dout['interview_date'] = pd.to_datetime(dout.interview_date).dt.strftime('%m/%d/%Y')
+    # now export
+    crosswalk_subset.reset_index(inplace=True)
+    strucroot = crosswalk_subset.nda_structure.str.strip().str[:-2][0]
+    strucnum = crosswalk_subset.nda_structure.str.strip().str[-2:][0]
+    # finalsubset - i.e. no withdraws
+    # subjectkey	src_subject_id	interview_age	interview_date	gender
+    filePath = os.path.join(pathstructuresout, 'HCPD_' + strucroot + strucnum + '_' + snapshotdate + '.csv')
+    if os.path.exists(filePath):
+        os.remove(filePath)
+    else:
+        pass
+        # print("Can not delete the file as it doesn't exists")
+
+    with open(filePath, 'a') as f:
+        f.write(strucroot + "," + str(int(strucnum)) + "\n")
+        dout.to_csv(f, index=False)

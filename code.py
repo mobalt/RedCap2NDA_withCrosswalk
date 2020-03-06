5af0024ee73442183c1352ecc2a6af8271a1ce77
Moises Baltazar <me@moisesb.com>
2020-03-06 13:32:53 -0600

    Implement more concise decrement function

--- code.py
+++ code.py
@@ -226,17 +226,11 @@ def mctq_free9(studydata, column, context):
     }) + studydata.mctq_free10
 fn["mctq_free9"]=mctq_free9
 
-def neo1(studydata, column, context):
+def __decrement_1_5_to_0_4(studydata, column, context):
     """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo1=pd.to_numeric(studydata.neo1,errors='coerce')-1
-    studydata.neo1=neo1.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo1"]=neo1
-
-def neo2(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo2=pd.to_numeric(studydata.neo2,errors='coerce')-1
-    studydata.neo2=neo2.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo2"]=neo2
+    #n=pd.to_numeric(column, errors='coerce')-1
+    #return n.fillna(-99).astype(int).astype(str).str.replace('-99','')
+    return column.replace({5: 4, 4: 3, 3: 2, 2: 1, 1: 0})
 
 def neo3(studydata, column, context):
     """Please decrement your 1::5 scale to a 0::4 scale, reverse-coding where the manual calls for it, and recalculating scores accordingly."""
@@ -244,155 +238,11 @@ def neo3(studydata, column, context):
     studydata.neo3=neo3.fillna(-99).astype(int).astype(str).str.replace('-99','')
 fn["neo3"]=neo3
 
-def neo4(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo4=pd.to_numeric(studydata.neo4,errors='coerce')-1
-    studydata.neo4=neo4.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo4"]=neo4
-
-def neo5(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo5=pd.to_numeric(studydata.neo5,errors='coerce')-1
-    studydata.neo5=neo5.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo5"]=neo5
-
-def neo6(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo6=pd.to_numeric(studydata.neo6,errors='coerce')-1
-    studydata.neo6=neo6.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo6"]=neo6
-
-def neo7(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo7=pd.to_numeric(studydata.neo7,errors='coerce')-1
-    studydata.neo7=neo7.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo7"]=neo7
-
 def neo8(studydata, column, context):
     """Please decrement your 1::5 scale to a 0::4 scale, reverse-coding where the manual calls for it, and recalculating scores accordingly."""
     neo8=4-(pd.to_numeric(studydata.neo8,errors='coerce')-1)
     studydata.neo8=neo8.fillna(-99).astype(int).astype(str).str.replace('-99','')
 fn["neo8"]=neo8
-
-def neo9(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo9=pd.to_numeric(studydata.neo9,errors='coerce')-1
-    studydata.neo9=neo9.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo9"]=neo9
-
-def neo10(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo10=pd.to_numeric(studydata.neo10,errors='coerce')-1
-    studydata.neo10=neo10.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo10"]=neo10
-
-def neo11(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo11=pd.to_numeric(studydata.neo11,errors='coerce')-1
-    studydata.neo11=neo11.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo11"]=neo11
-
-def neo12(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo12=pd.to_numeric(studydata.neo12,errors='coerce')-1
-    studydata.neo12=neo12.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo12"]=neo12
-
-def neo13(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo13=pd.to_numeric(studydata.neo13,errors='coerce')-1
-    studydata.neo13=neo13.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo13"]=neo13
-
-def neo14(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo14=pd.to_numeric(studydata.neo14,errors='coerce')-1
-    studydata.neo14=neo14.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo14"]=neo14
-
-def neo15(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo15=pd.to_numeric(studydata.neo15,errors='coerce')-1
-    studydata.neo15=neo15.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo15"]=neo15
-
-def neo16(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo16=pd.to_numeric(studydata.neo16,errors='coerce')-1
-    studydata.neo16=neo16.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo16"]=neo16
-
-def neo17(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo17=pd.to_numeric(studydata.neo17,errors='coerce')-1
-    studydata.neo17=neo17.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo17"]=neo17
-
-def neo18(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo18=pd.to_numeric(studydata.neo18,errors='coerce')-1
-    studydata.neo18=neo18.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo18"]=neo18
-
-def neo19(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo19=pd.to_numeric(studydata.neo19,errors='coerce')-1
-    studydata.neo19=neo19.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo19"]=neo19
-
-def neo20(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo20=pd.to_numeric(studydata.neo20,errors='coerce')-1
-    studydata.neo20=neo20.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo20"]=neo20
-
-def neo21(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo21=pd.to_numeric(studydata.neo21,errors='coerce')-1
-    studydata.neo21=neo21.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo21"]=neo21
-
-def neo22(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo22=pd.to_numeric(studydata.neo22,errors='coerce')-1
-    studydata.neo22=neo22.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo22"]=neo22
-
-def neo23(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo23=pd.to_numeric(studydata.neo23,errors='coerce')-1
-    studydata.neo23=neo23.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo23"]=neo23
-
-def neo24(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo24=pd.to_numeric(studydata.neo24,errors='coerce')-1
-    studydata.neo24=neo24.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo24"]=neo24
-
-def neo25(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo25=pd.to_numeric(studydata.neo25,errors='coerce')-1
-    studydata.neo25=neo25.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo25"]=neo25
-
-def neo26(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo26=pd.to_numeric(studydata.neo26,errors='coerce')-1
-    studydata.neo26=neo26.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo26"]=neo26
-
-def neo27(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo27=pd.to_numeric(studydata.neo27,errors='coerce')-1
-    studydata.neo27=neo27.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo27"]=neo27
-
-def neo28(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo28=pd.to_numeric(studydata.neo28,errors='coerce')-1
-    studydata.neo28=neo28.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo28"]=neo28
 
 def neo29(studydata, column, context):
     """Please decrement your 1::5 scale to a 0::4 scale, reverse-coding where the manual calls for it, and recalculating scores accordingly."""
@@ -400,191 +250,69 @@ def neo29(studydata, column, context):
     studydata.neo29=neo29.fillna(-99).astype(int).astype(str).str.replace('-99','')
 fn["neo29"]=neo29
 
-def neo30(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo30=pd.to_numeric(studydata.neo30,errors='coerce')-1
-    studydata.neo30=neo30.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo30"]=neo30
-
-def neo31(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo31=pd.to_numeric(studydata.neo31,errors='coerce')-1
-    studydata.neo31=neo31.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo31"]=neo31
-
-def neo32(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo32=pd.to_numeric(studydata.neo32,errors='coerce')-1
-    studydata.neo32=neo32.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo32"]=neo32
-
-def neo33(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo33=pd.to_numeric(studydata.neo33,errors='coerce')-1
-    studydata.neo33=neo33.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo33"]=neo33
-
-def neo34(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo34=pd.to_numeric(studydata.neo34,errors='coerce')-1
-    studydata.neo34=neo34.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo34"]=neo34
-
-def neo35(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo35=pd.to_numeric(studydata.neo35,errors='coerce')-1
-    studydata.neo35=neo35.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo35"]=neo35
-
-def neo36(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo36=pd.to_numeric(studydata.neo36,errors='coerce')-1
-    studydata.neo36=neo36.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo36"]=neo36
-
-def neo37(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo37=pd.to_numeric(studydata.neo37,errors='coerce')-1
-    studydata.neo37=neo37.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo37"]=neo37
-
 def neo38(studydata, column, context):
     """Please decrement your 1::5 scale to a 0::4 scale, reverse-coding where the manual calls for it, and recalculating scores accordingly."""
     neo38=4-(pd.to_numeric(studydata.neo38,errors='coerce')-1)
     studydata.neo38=neo38.fillna(-99).astype(int).astype(str).str.replace('-99','')
 fn["neo38"]=neo38
 
-def neo39(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo39=pd.to_numeric(studydata.neo39,errors='coerce')-1
-    studydata.neo39=neo39.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo39"]=neo39
 
-def neo40(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo40=pd.to_numeric(studydata.neo40,errors='coerce')-1
-    studydata.neo40=neo40.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo40"]=neo40
-
-def neo41(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo41=pd.to_numeric(studydata.neo41,errors='coerce')-1
-    studydata.neo41=neo41.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo41"]=neo41
-
-def neo42(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo42=pd.to_numeric(studydata.neo42,errors='coerce')-1
-    studydata.neo42=neo42.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo42"]=neo42
-
-def neo43(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo43=pd.to_numeric(studydata.neo43,errors='coerce')-1
-    studydata.neo43=neo43.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo43"]=neo43
-
-def neo44(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo44=pd.to_numeric(studydata.neo44,errors='coerce')-1
-    studydata.neo44=neo44.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo44"]=neo44
-
-def neo45(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo45=pd.to_numeric(studydata.neo45,errors='coerce')-1
-    studydata.neo45=neo45.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo45"]=neo45
-
-def neo46(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo46=pd.to_numeric(studydata.neo46,errors='coerce')-1
-    studydata.neo46=neo46.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo46"]=neo46
-
-def neo47(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo47=pd.to_numeric(studydata.neo47,errors='coerce')-1
-    studydata.neo47=neo47.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo47"]=neo47
-
-def neo48(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo48=pd.to_numeric(studydata.neo48,errors='coerce')-1
-    studydata.neo48=neo48.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo48"]=neo48
-
-def neo49(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo49=pd.to_numeric(studydata.neo49,errors='coerce')-1
-    studydata.neo49=neo49.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo49"]=neo49
-
-def neo50(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo50=pd.to_numeric(studydata.neo50,errors='coerce')-1
-    studydata.neo50=neo50.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo50"]=neo50
-
-def neo51(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo51=pd.to_numeric(studydata.neo51,errors='coerce')-1
-    studydata.neo51=neo51.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo51"]=neo51
-
-def neo52(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo52=pd.to_numeric(studydata.neo52,errors='coerce')-1
-    studydata.neo52=neo52.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo52"]=neo52
-
-def neo53(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo53=pd.to_numeric(studydata.neo53,errors='coerce')-1
-    studydata.neo53=neo53.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo53"]=neo53
-
-def neo54(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo54=pd.to_numeric(studydata.neo54,errors='coerce')-1
-    studydata.neo54=neo54.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo54"]=neo54
-
-def neo55(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo55=pd.to_numeric(studydata.neo55,errors='coerce')-1
-    studydata.neo55=neo55.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo55"]=neo55
-
-def neo56(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo56=pd.to_numeric(studydata.neo56,errors='coerce')-1
-    studydata.neo56=neo56.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo56"]=neo56
-
-def neo57(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo57=pd.to_numeric(studydata.neo57,errors='coerce')-1
-    studydata.neo57=neo57.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo57"]=neo57
-
-def neo58(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo58=pd.to_numeric(studydata.neo58,errors='coerce')-1
-    studydata.neo58=neo58.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo58"]=neo58
-
-def neo59(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo59=pd.to_numeric(studydata.neo59,errors='coerce')-1
-    studydata.neo59=neo59.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo59"]=neo59
-
-def neo60(studydata, column, context):
-    """Please decrement your 1::5 scale to a 0::4 scale"""
-    neo60=pd.to_numeric(studydata.neo60,errors='coerce')-1
-    studydata.neo60=neo60.fillna(-99).astype(int).astype(str).str.replace('-99','')
-fn["neo60"]=neo60
+fn["neo1"]=__decrement_1_5_to_0_4
+fn["neo2"]=__decrement_1_5_to_0_4
+fn["neo4"]=__decrement_1_5_to_0_4
+fn["neo5"]=__decrement_1_5_to_0_4
+fn["neo6"]=__decrement_1_5_to_0_4
+fn["neo7"]=__decrement_1_5_to_0_4
+fn["neo9"]=__decrement_1_5_to_0_4
+fn["neo10"]=__decrement_1_5_to_0_4
+fn["neo11"]=__decrement_1_5_to_0_4
+fn["neo12"]=__decrement_1_5_to_0_4
+fn["neo13"]=__decrement_1_5_to_0_4
+fn["neo14"]=__decrement_1_5_to_0_4
+fn["neo15"]=__decrement_1_5_to_0_4
+fn["neo16"]=__decrement_1_5_to_0_4
+fn["neo17"]=__decrement_1_5_to_0_4
+fn["neo18"]=__decrement_1_5_to_0_4
+fn["neo19"]=__decrement_1_5_to_0_4
+fn["neo20"]=__decrement_1_5_to_0_4
+fn["neo21"]=__decrement_1_5_to_0_4
+fn["neo22"]=__decrement_1_5_to_0_4
+fn["neo23"]=__decrement_1_5_to_0_4
+fn["neo24"]=__decrement_1_5_to_0_4
+fn["neo25"]=__decrement_1_5_to_0_4
+fn["neo26"]=__decrement_1_5_to_0_4
+fn["neo27"]=__decrement_1_5_to_0_4
+fn["neo28"]=__decrement_1_5_to_0_4
+fn["neo30"]=__decrement_1_5_to_0_4
+fn["neo31"]=__decrement_1_5_to_0_4
+fn["neo32"]=__decrement_1_5_to_0_4
+fn["neo33"]=__decrement_1_5_to_0_4
+fn["neo34"]=__decrement_1_5_to_0_4
+fn["neo35"]=__decrement_1_5_to_0_4
+fn["neo36"]=__decrement_1_5_to_0_4
+fn["neo37"]=__decrement_1_5_to_0_4
+fn["neo39"]=__decrement_1_5_to_0_4
+fn["neo40"]=__decrement_1_5_to_0_4
+fn["neo41"]=__decrement_1_5_to_0_4
+fn["neo42"]=__decrement_1_5_to_0_4
+fn["neo43"]=__decrement_1_5_to_0_4
+fn["neo44"]=__decrement_1_5_to_0_4
+fn["neo45"]=__decrement_1_5_to_0_4
+fn["neo46"]=__decrement_1_5_to_0_4
+fn["neo47"]=__decrement_1_5_to_0_4
+fn["neo48"]=__decrement_1_5_to_0_4
+fn["neo49"]=__decrement_1_5_to_0_4
+fn["neo50"]=__decrement_1_5_to_0_4
+fn["neo51"]=__decrement_1_5_to_0_4
+fn["neo52"]=__decrement_1_5_to_0_4
+fn["neo53"]=__decrement_1_5_to_0_4
+fn["neo54"]=__decrement_1_5_to_0_4
+fn["neo55"]=__decrement_1_5_to_0_4
+fn["neo56"]=__decrement_1_5_to_0_4
+fn["neo57"]=__decrement_1_5_to_0_4
+fn["neo58"]=__decrement_1_5_to_0_4
+fn["neo59"]=__decrement_1_5_to_0_4
+fn["neo60"]=__decrement_1_5_to_0_4
 
 def dummy_var15(studydata, column, context):
     """Please use the 'version_form' column to label all rows "S7up Self Report"."""
